97d95daf0a2cb5f3ecce72e98757287e6019beb5
eacallam <eacallam@yahoo.com>
2017-11-19 11:04:05 -0500

    Additional work completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+$(document).ready(function () {
+    $('#1').on('click', function () {
+        $("#headerPicture").attr('src', "img/mainPicture.png");
+    });
+
+    $('#2').on('click', function () {
+        $("#headerPicture").attr('src', "img/karaokePicture.png");
+    });
+
+    $('#3').on('click', function () {
+        $("#headerPicture").attr('src', "img/jPicture.png");
+    });
+
+    $('#4').on('click', function () {
+        $("#headerPicture").attr('src', "img/youngElizabeth.png");
+    });
+
+
+});
+
+
+
+var showText = function (target, message, index, interval) {
+    if (index < message.length) {
+        $(target).append(message[index++]);
+        setTimeout(function () { showText(target, message, index, interval); }, interval);
+    }
+}
+
+$(function () {
+    showText("#headerName", "elizabeth callam", 0, 150);
+
+        });
+
 
