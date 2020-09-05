f2154d670fd68289b1f022aa272c82f37e66b19f
Beth Callam <bethcallam2@gmail.com>
2020-09-05 18:10:33 -0400

    Add more site content

--- js/main.js
+++ js/main.js
@@ -29,5 +29,49 @@ var showText = function (target, message, index, interval) {
 
 $(function () {
     showText("#headerName", "beth callam", 0, 150);
+});
 
-        });
+function openFirstModal() {
+    document.getElementById("resumeModalFirst").style.display = "block";
+}
+  
+function closeFirstModal() {
+    document.getElementById("resumeModalFirst").style.display = "none";
+}
+
+function openSecondModal() {
+    document.getElementById("resumeModalSecond").style.display = "block";
+}
+  
+function closeSecondModal() {
+    document.getElementById("resumeModalSecond").style.display = "none";
+}
+
+function openThirdModal() {
+    document.getElementById("resumeModalThird").style.display = "block";
+}
+  
+function closeThirdModal() {
+    document.getElementById("resumeModalThird").style.display = "none";
+}
+  
+var slideIndex = 1;
+
+function currentSlide(n, className) {
+  showSlides(slideIndex = n, className);
+}
+
+function plusSlides(n, className) {
+  showSlides(slideIndex += n, className);
+}
+  
+function showSlides(n, className) {
+    var i;
+    var slides = document.getElementsByClassName(className);
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
